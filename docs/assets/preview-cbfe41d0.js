541f0b6c611805bb994f66cceb38793661bdd33f
yoshi6jp <yoshi6jp@users.noreply.github.com>
2023-11-25 00:51:32 +0000

    docs: update storybook for latest release

---
+++ docs/assets/preview-cbfe41d0.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-211c57b2.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-2513d390.js"),["./DocsRenderer-3PUGWF3O-2513d390.js","./iframe-211c57b2.js","./index-ebeaab24.js","./react-18-8ac2d3a8.js","./index-073301bc.js","./index-160a66d6.js","./chunk-H6ZRF7XV-146ce0bc.js","./index-d475d2ea.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-cbfe41d0.js.map
---
+++ docs/assets/syntaxhighlighter-2MZ4Y3HO-d8e24849.js
@@ -0,0 +1,2 @@
+import{S as c,c as l,s as n}from"./index-160a66d6.js";import"./iframe-211c57b2.js";import"../sb-preview/runtime.js";import"./chunk-H6ZRF7XV-146ce0bc.js";import"./index-d475d2ea.js";import"./index-ebeaab24.js";import"./index-073301bc.js";import"./index-356e4a49.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
+//# sourceMappingURL=syntaxhighlighter-2MZ4Y3HO-d8e24849.js.map
