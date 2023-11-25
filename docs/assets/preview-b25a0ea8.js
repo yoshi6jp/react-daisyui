161fcb20f51208756872e7edfd87b458db357e86
yoshi6jp <yoshi6jp@users.noreply.github.com>
2023-11-25 00:47:09 +0000

    docs: update storybook for latest release

---
+++ docs/assets/preview-b25a0ea8.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-6bd7b356.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-9bc33319.js"),["./DocsRenderer-3PUGWF3O-9bc33319.js","./iframe-6bd7b356.js","./index-ebeaab24.js","./react-18-8ac2d3a8.js","./index-073301bc.js","./index-15f52aee.js","./chunk-H6ZRF7XV-146ce0bc.js","./index-d475d2ea.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-b25a0ea8.js.map
